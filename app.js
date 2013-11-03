1caccc0b6af8b4ceecfd1adb653ee62860736f81
iceiony <iceiony@yahoo.com>
2013-11-03 19:20:03 +0000

    record wave data at each key press

--- app.js
+++ app.js
@@ -1,28 +1,62 @@
-var net = require('net');
+var net = require('net'),
+    keypress = require('keypress'),
+    fs = require('fs');
 
-var socket = new net.Socket();
+
+var socket = new net.Socket(),
+    data = [],
+    trialData = [],
+    record = false;
+
+keypress(process.stdin);
+process.stdin.on('keypress', function (ch, key) {
+    if (key && key.name === 'up') {
+        record = true;
+        setTimeout(function () {
+            data.push(trialData);
+            trialData = [];
+            record = false;
+        }, 500);
+    }
+
+    if (key && key.ctrl && key.name == 'c') {
+        //todo: end the process and save all data
+        fs.writeFile("data.txt", JSON.stringify(data), function () {
+            process.exit();
+        });
+    }
+});
+//webstorm loads the process in a container and overwrites stdin stream
+if (process.stdin.constructor.name.toLowerCase() !== 'socket') {
+    process.stdin.setRawMode(true);
+}
+process.stdin.resume();
 
 
-socket.setEncoding('ascii');
-socket.on('error',function(error){
+socket.on('error', function (error) {
     console.log(error);
 })
 
-socket.on('data',function(buffer){
-    console.log(buffer);
+socket.on('data', function (buffer) {
+    if (record) {
+        console.log(buffer);
+        trialData.push( buffer );
+    }
 })
 
-socket.connect(13854,'127.0.0.1',function(){
+socket.setEncoding('ascii');
+socket.connect(13854, '127.0.0.1', function () {
     var configuration = JSON.stringify({
-        enableRawOutput : true,
-        format : "Json"
+        enableRawOutput: true,
+        format: "Json"
     });
 
     console.log('Connected ! ');
     console.log(arguments);
-    console.log('Sending configuration :' + configuration );
+    console.log('Sending configuration :' + JSON.stringify(configuration));
 
-    socket.write(configuration, "utf-8",function(){
-        console.log(arguments);
+    socket.write(configuration, "utf-8", function () {
+        console.log("Configuration sent" + JSON.stringify(arguments));
     });
+
 });
