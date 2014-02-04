95a9891e1ab841bc771df818454fa60849122c6c
adrian.ionita <adrian.ionita@wonga.com>
2014-02-04 16:51:25 +0000

    update test image to use rgb instead of hex

--- testImg.js
+++ testImg.js
@@ -1,11 +1,13 @@
 // gm - Copyright Aaron Heckmann <aaron.heckmann+github@gmail.com> (MIT Licensed)
 
 var gm = require('gm');
-var image = gm(525, 110, "#00ff55")
+var image = gm(525, 110, "rgb(0,200,0)")
 
   image
-  .fill("#2c2")
+  .fill("rgb(0,-34,0)")
   .drawRectangle(40, 10, 50, 20)
+  .fill("rgb(0,221,0)")
+  .drawRectangle(60, 10, 70, 20)
   .write('drawing.png', function(err){
 	if (err) return console.dir(arguments)
     console.log(this.outname + ' created :: ' + arguments[3])
