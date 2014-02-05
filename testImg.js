67540017248288218eb31de706fbe76483cbf290
iceiony <iceiony@yahoo.com>
2014-02-05 10:24:00 +0000

    write new test image with new canvas api

--- testImg.js
+++ testImg.js
@@ -1,15 +1,23 @@
 // gm - Copyright Aaron Heckmann <aaron.heckmann+github@gmail.com> (MIT Licensed)
 
-var gm = require('gm');
-var image = gm(525, 110, "rgb(0,200,0)")
+var Canvas = require('canvas'),
+    canvas = new Canvas(525,110),
+    ctx = canvas.getContext("2d");
 
-  image
-  .fill("rgb(0,-34,0)")
-  .drawRectangle(40, 10, 50, 20)
-  .fill("rgb(0,221,0)")
-  .drawRectangle(60, 10, 70, 20)
-  .write('drawing.png', function(err){
-	if (err) return console.dir(arguments)
-    console.log(this.outname + ' created :: ' + arguments[3])
-  }
-) 
+var fs = require('fs'),
+    out = fs.createWriteStream("./drawing.png"),
+    stream = canvas.createPNGStream();
+
+  ctx.fillStyle = "rgb(0,125,0)";
+  ctx.fillRect(0,0, canvas.width,canvas.height);
+  ctx.fillStyle = "rgb(0,200,0)";
+  ctx.fillRect(40, 10, 10, 10);
+  ctx.fillRect(60, 10, 10, 10);
+
+stream.on('data',function(chunk){
+    out.write(chunk);
+});
+
+stream.on('end',function(end){
+    out.end();
+});
